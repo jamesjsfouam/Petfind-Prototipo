3afd162f3e903538f36e2059fc223efa2688c5d2
Jamesjsfo <jsfernandez@uamv.edu.ni>
2026-06-14 11:23:00 -0600

    estructuras agregadas

--- Petfind-Prototipo/Program.cs
+++ Petfind-Prototipo/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿struct Usuario
+{
+    public string nombre;
+    public string correo;
+    public string telefono;
+    public string clave;
+    public int petPoints;
+} 
+
+struct Mascota
+{
+    public int id;
+    public string duenoCorreo;
+    public string nombre;
+    public string especie;
+    public string raza;
+    public string color;
+    public string sangre;
+    public string alergias;
+    public string vacunas;
+    public bool extraviada;
+}
